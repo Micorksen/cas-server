5414a0ef6d2913ce1630f064133acddbc6e5451f
Matteo C <hello@micorksen.xyz>
2022-03-12 19:51:35 +0100

    Fix typos.

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -1,9 +1,9 @@
-@extends('casserver::layout')
-@section('content')
+@extends("cas-server::layout")
+@section("content")
 <div class="card error">
     <div class="error">
-        <h2 class="title">{{ $errorTitle }}</h2>
-        <p>{{$error}}</p>
+        <h2 class="title">{{ $title }}</h2>
+        <p>{{ $description }}</p>
     </div>
 </div>
 @endsection
