551244bb0a9f6b8196211e52119e5b361e8f33c7
Matteo C <hello@micorksen.xyz>
2022-03-12 19:56:44 +0100

    Fix typos.

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -1,11 +1,11 @@
-@extends('casserver::layout')
-@section('content')
+@extends("cas-server::layout")
+@section("content")
     <div class="card success">
         <div class="service">
-            <h2 class="title">Connexion réussie</h2>
-            <p>Vous êtes connecté en tant que {{$user}}.</p>
-            <p>Malheureusement, nous n'avons pas pu trouver le service auquel vous rediriger.</p>
-            <p>Pour votre sécurité, merci de vous <a href="/logout">déconnecter</a> et fermer votre navigateur lorsque vous avez fini d'utiliser ce service.</p>
+            <h2 class="title">Successful login</h2>
+            <p>You are logged in as {{ $user }}.</p>
+            <p>Unfortunately, we were unable to find the service to redirect you to.</p>
+            <p>For your safety, please <a href="/logout">log out</a> and close your browser when you are finished using this service.</p>
         </div>
     </div>
 @endsection
